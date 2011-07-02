491a2391c63005ed1ac29ce0f15f3f363dcab431
Eran Meir <eranmeir@gmail.com>
2011-07-02 13:27:37 +0300

    Trying some basic XML to JSON conversion

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2007 Google Inc.
+# Copyright 2011 xbrl-il.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,12 +22,60 @@ class MainHandler(webapp.RequestHandler):
     def get(self):
         self.response.out.write('Hello world!')
 
+from google.appengine.api import urlfetch                      
+
+from xml.etree import ElementTree as etree
+from xml.etree.ElementTree import parse
+import pesterfish
+
+class XML2JSON(webapp.RequestHandler):
+    _someString =  """<?xml version="1.0" encoding="UTF-8"?>
+<ISBNdb server_time="2005-02-25T23:03:41">
+ <BookList total_results="1" page_size="10" page_number="1" shown_results="1">
+  <BookData book_id="somebook" isbn="0123456789">
+   <Title>Interesting Book</Title>
+   <TitleLong>Interesting Book: Read it or else..</TitleLong>
+   <AuthorsText>John Doe</AuthorsText>
+   <PublisherText>Acme Publishing</PublisherText>
+  </BookData>
+ </BookList>
+</ISBNdb>"""
+    def get(self):
+        self.response.headers['Content-Type'] = "text/plain"
+        source = self.request.get("url")
+        xml=self._someString
+        if (len(source) > 0):
+            #source = "http://www.w3schools.com/XML/note.xml"
+            #source = "http://localhost:8080/xbrl/File2011-01-155475.xbrl"
+            # [Eran] TODO - avoid recursion
+            try:
+                result = urlfetch.fetch(url=source, deadline=10)
+            except (ValueError, IOError, OSError,urlfetch.Error) as e:
+                self.response.out.write("exception: " + repr(e) + "\n")
+                self.response.out.write("Bad url: " + source + "\nUsing hardcoded string instead\n")
+            if (result.status_code == 200):
+                self.response.out.write("Parsing XML from url: " + source + "\n")
+                xml = result.content
+        else:
+            self.response.out.write("Parsing some hard-coded string\n")
+
+        try:                                  
+            tree = etree.fromstring(xml)
+            json = pesterfish.to_pesterfish(tree)
+        except (TypeError):            
+            json = "pesterfish failed"         
+
+        self.response.out.write('Json result:\n' + json)
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
+    application = webapp.WSGIApplication([
+        ('/', MainHandler),
+        ('/test', XML2JSON),
+        ],
                                          debug=True)
     util.run_wsgi_app(application)
 
 
 if __name__ == '__main__':
     main()
+
